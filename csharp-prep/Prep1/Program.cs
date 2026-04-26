2cd541a464df0129172b9513e5303e7afb89c7ce
thekiriya <phi22009@byui.edu>
2026-04-25 21:49:59 -0600

    prep1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+       Console.WriteLine("What is your first name? "); 
+       string fname = Console.ReadLine(); 
+
+       Console.WriteLine("What is your last name? ");
+       string lname = Console.ReadLine(); 
+
+       Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        
     }
 }
+
+
