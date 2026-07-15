b82347ae39aa3aae27d4baa01dd9dfc8efa4f54f
thekiriya <phi22009@byui.edu>
2026-07-14 22:57:09 -0600

    resume unit activity

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,18 @@
+
+using System.Runtime.CompilerServices;
+
+public class Job
+
+{
+    
+   public string _companyName = ""; 
+   public string _jobTitle = ""; 
+   public int _startYear; 
+   public int _endYear; 
+
+   public void Display()
+
+    {
+        Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}"); 
+    }
+}
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,24 @@
+using System; 
+
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+public class Resume
+
+{
+    
+    public string _personName = ""; 
+
+    public List <Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_personName}"); 
+        Console.WriteLine("Jobs"); 
+
+        foreach (Job job in _jobs)
+            {
+            job.Display(); 
+        }
+    }
+}
