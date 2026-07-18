581f6def4a98390232a5b93b1aa493893bd998c4
thekiriya <phi22009@byui.edu>
2026-07-18 16:04:28 -0600

    prepare 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
+using ShapeProgram;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Test Square
+        Square square = new Square("Red", 5);
+        Console.WriteLine($"Square - Color: {square.GetColor()}, Area: {square.GetArea():F2}");
+
+        // Test Rectangle
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Console.WriteLine($"Rectangle - Color: {rectangle.GetColor()}, Area: {rectangle.GetArea():F2}");
+
+        // Test Circle
+        Circle circle = new Circle("Green", 3);
+        Console.WriteLine($"Circle - Color: {circle.GetColor()}, Area: {circle.GetArea():F2}");
+
+        Console.WriteLine("\n--- List of Shapes ---");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+        shapes.Add(new Square("Yellow", 2.5));
+        shapes.Add(new Rectangle("Purple", 7, 3));
+        shapes.Add(new Circle("Orange", 4.2));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
     }
 }
